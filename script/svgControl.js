d58cacccbd0553041f0614b59d6fa3725317fc57
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2023-07-31 08:16:50 +0900

    기본 컨셉 확인

--- script/svgControl.js
+++ script/svgControl.js
@@ -1,99 +1,67 @@
 const fileDir = './assets/';
-// var svgFile = 'top01.svg';
-
-// // fetch API를 사용하여 SVG 파일 불러오기
-// fetch(`${fileDir}${svgFile}`)
-//   .then((response) => response.text())
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((error) => {
-//     console.error('Failed to load SVG file', error);
-//   });
-
-const charmTop = {
-  idx: 0,
-  fileNameList: ['top01.svg', 'top02.svg'],
-  svgs: [],
-  container: null,
-};
-const charmMid = {
-  idx: 0,
-  fileNameList: ['mid01.svg', 'mid02.svg'],
-  svgs: [],
-  container: null,
-};
-const charmBot = {
-  idx: 0,
-  fileNameList: ['bot01.svg', 'bot02.svg'],
-  svgs: [],
-  container: null,
-};
+class Charm {
+  constructor(continaerClassName, fileList) {
+    this.idx = 0;
+    this.fileNameList = fileList;
+    this.svgs = [];
+    this.containerClass = continaerClassName;
+    this.container = null;
+  }
+  inc = () => {
+    this.idx = this.idx + 1;
+    if (this.idx >= this.svgs.length) this.idx = 0;
+    this.display();
+  };
+  dec = () => {
+    this.idx = this.idx - 1;
+    if (this.idx < 0) this.idx = this.svgs.length - 1;
+    this.display();
+  };
+  display = () => {
+    this.container.innerHTML = this.svgs[this.idx];
+  };
+}
+const charmTop = new Charm('charm-top', ['top01.svg', 'top02.svg']);
+const charmMid = new Charm('charm-mid', ['mid01.svg', 'mid02.svg']);
+const charmBot = new Charm('charm-bot', ['bot01.svg', 'bot02.svg']);
+const charmsArry = [charmTop, charmMid, charmBot];
 
 const setCharmCotainer = () => {
-  charmTop.container = document.querySelector('.charm-top');
-  charmMid.container = document.querySelector('.charm-mid');
-  charmBot.container = document.querySelector('.charm-bot');
-};
-
-const incButtonIdxTop = () => {
-  charmTop.idx = charmTop.idx + 1;
-  if (charmTop.idx >= charmTop.svgs.length) charmTop.idx = 0;
-  displayCharm(charmTop);
-};
-const incButtonIdxMid = () => {
-  charmMid.idx = charmMid.idx + 1;
-  if (charmMid.idx >= charmMid.svgs.length) charmMid.idx = 0;
-  displayCharm(charmMid);
-};
-const incButtonIdxBot = () => {
-  charmBot.idx = charmBot.idx + 1;
-  if (charmBot.idx >= charmBot.svgs.length) charmBot.idx = 0;
-  displayCharm(charmBot);
-};
-const decButtonIdxTop = () => {
-  charmTop.idx = charmTop.idx - 1;
-  if (charmTop.idx < 0) charmTop.idx = charmTop.svgs.length - 1;
-  displayCharm(charmTop);
-};
-const decButtonIdxMid = () => {
-  charmMid.idx = charmMid.idx - 1;
-  if (charmMid.idx < 0) charmMid.idx = charmMid.svgs.length - 1;
-  displayCharm(charmMid);
-};
-const decButtonIdxBot = () => {
-  charmBot.idx = charmBot.idx - 1;
-  if (charmBot.idx < 0) charmBot.idx = charmBot.svgs.length - 1;
-  displayCharm(charmBot);
+  charmsArry.forEach((eachCharm) => {
+    eachCharm.container = document.querySelector(
+      `.${eachCharm.containerClass}`
+    );
+  });
 };
 
 const setButton = () => {
   document
     .querySelector('.button.button-left.button-top')
-    .addEventListener('click', decButtonIdxTop);
+    .addEventListener('click', charmTop.dec);
   document
     .querySelector('.button.button-left.button-mid')
-    .addEventListener('click', decButtonIdxMid);
+    .addEventListener('click', charmMid.dec);
   document
     .querySelector('.button.button-left.button-bot')
-    .addEventListener('click', decButtonIdxBot);
+    .addEventListener('click', charmBot.dec);
   document
     .querySelector('.button.button-right.button-top')
-    .addEventListener('click', incButtonIdxTop);
+    .addEventListener('click', charmTop.inc);
   document
     .querySelector('.button.button-right.button-mid')
-    .addEventListener('click', incButtonIdxMid);
+    .addEventListener('click', charmMid.inc);
   document
     .querySelector('.button.button-right.button-bot')
-    .addEventListener('click', incButtonIdxBot);
+    .addEventListener('click', charmBot.inc);
 };
 
-const fetchFiles = (charmDb) => {
-  charmDb.fileNameList.forEach((fileName) => {
+const fetchFiles = (charm) => {
+  charm.fileNameList.forEach((fileName) => {
     fetch(`${fileDir}${fileName}`)
       .then((response) => response.text())
       .then((data) => {
-        charmDb.svgs.push(data);
+        charm.svgs.push(data);
+        if (charm.svgs.length === 1) charm.display();
       })
       .catch((error) => {
         console.error('Failed to load SVG file', error);
@@ -101,22 +69,20 @@ const fetchFiles = (charmDb) => {
   });
 };
 
-const displayCharm = (charmDb) => {
-  charmDb.container.innerHTML = charmDb.svgs[charmDb.idx];
-};
-
-const init = () => {
-  displayCharm(charmTop);
-  displayCharm(charmMid);
-  displayCharm(charmBot);
-};
-
 setCharmCotainer();
 setButton();
-fetchFiles(charmTop);
-fetchFiles(charmMid);
-fetchFiles(charmBot);
-setTimeout(init, 3000);
+charmsArry.forEach((eachCharm) => {
+  fetchFiles(eachCharm);
+});
 console.log(charmTop);
 console.log(charmMid);
 console.log(charmBot);
+
+const controlStrk = document.querySelector('#control-strk-w');
+controlStrk.addEventListener('input', (evt) => {
+  // console.log(evt);
+  document.documentElement.style.setProperty(
+    '--stroke-w-normal',
+    evt.target.value
+  );
+});
